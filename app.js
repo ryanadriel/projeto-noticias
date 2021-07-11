5001963dae2820b986566490187f142a83533b34
ryanadriel <adrielryan.tj@hotmail.com>
2021-07-11 16:55:35 -0300

    (fim dos modulos)

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
 
-var rotaHome = require('./app/routes/home');
-rotaHome(app);
+var rotaHome = require('./app/routes/home')(app);
 
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia');
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
     console.log("Servidor On");
