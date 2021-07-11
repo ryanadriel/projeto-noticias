fa5082b918e732dcbbf498f93636f634384eba39
ryanadriel <adrielryan.tj@hotmail.com>
2021-07-11 15:47:18 -0300

    (modulando)

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 var app = require('./config/server');
 
-app.get('/', function(req, res){
-    res.render("home/index");
-});
-
-app.get('/formulario_inclusao_noticia', function(req, res){
-    res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(req, res){
-    res.render("noticias/noticias");
-});
+var rotaNoticias = require('./app/routes/noticias');
+rotaNoticias(app);
 
 app.listen(3000, function(){
     console.log("Servidor On");
---
+++ app/routes/formulario_inclusao_noticia.js
@@ -0,0 +1,3 @@
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render("admin/form_add_noticia");
+});
