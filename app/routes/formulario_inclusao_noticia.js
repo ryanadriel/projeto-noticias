6a5d34dc602fa50486f7a4a6636328d4c382ab27
ryanadriel <adrielryan.tj@hotmail.com>
2021-08-02 11:21:26 -0300

    resolvendo problema de interpretação

--- app/routes/formulario_inclusao_noticia.js
+++ app/routes/formulario_inclusao_noticia.js
@@ -3,4 +3,4 @@ module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(req, res){
         res.render("admin/form_add_noticia");
     });
-};
+}
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,20 @@
 module.exports = function(app){
 
-app.get('/noticias', function(req, res){
-    res.render("noticias/noticias");
+    app.get('/noticias', function(req, res){
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '1027',
+            database: 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function(error, result){
+            res.send(result);
+
+        });
+
+        //res.render("noticias/noticias");
     });
 };
